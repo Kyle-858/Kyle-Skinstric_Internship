245214f54f492694210eda3077edceb3c3ff8441
Kyle-858 <krazykyle19@gmail.com>
2025-09-02 01:56:09 -0400

    Homepage needs buttons

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-info">
+        <div className="header-logo">SKINSTRIC</div>
+        <div className="header-part">[ INTRO ]</div>
+      </div>
+      <button className="header-btn">ENTER CODE</button>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+        <h1 className="home-title">Sophisticated<br/>skincare</h1>
+        <p className="about-text">Skinstric developed an A.I. that creates 
+            <br/>a highly-personalised routine tailored to <br/>
+            what your skin needs.</p>
+    </div>
+  )
+}
+
+export default Home
